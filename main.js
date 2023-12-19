57a1a5448f0759605f254f2529e937f5989093a3
FrancoBarberis <francobarberissic3@gmail.com>
2023-12-19 16:29:51 -0300

    Funciones

--- main.js
+++ main.js
@@ -12,9 +12,15 @@ const producto3 = { id: 3, producto: "Harina", precio: 500 };
 const producto4 = { id: 4, producto: "Fideos", precio: 1200 };
 
 const productos = [];
-productos.push(producto1, producto2, producto3, producto4);
-console.log(productos);
 
+//DEFINO FUNCIONES
+function preguntarSiSigue() {
+    let sigue = prompt("¿Desea seguir agregando al carrito? (si/no)");
+    return sigue;
+}
+function alertarValidez() {
+    alert("Ingrese un valor válido (si/no)");
+}
 function agregarAlCarrito(idProducto, productos, carrito) {
     carrito.push(productos[nroProducto]);
     console.log("Producto agregado")
@@ -39,3 +45,18 @@ function quitarProducto(nroProducto, carrito){
     console.log(carrito);
     return;
 }
+
+productos.push(producto1.producto+" (Código 1), Precio: "+producto1.precio, producto2.producto+" (Código 2), Precio: "+producto2.precio, producto3.producto+" (Código 3), Precio: "+producto3.precio, producto4.producto+" (Código 4), Precio: "+producto4.precio);
+console.log(productos);
+let mensajeProductos = productos.join("\n"); //CREO EL MENSAJE CON SALTOS DE LINEA
+alert("Su presupuesto es: "+saldo+"\n"+"Estos son los productos disponibles con su precio: \n"+mensajeProductos);
+console.log(mensajeProductos);
+
+let códigoProd=parseInt(prompt("Ingrese el código de producto"));
+
+
+
+
+
+
+
